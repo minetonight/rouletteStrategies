5a934a2dd90b8f78127d6e4a44594f2ef05fbd9d
Aleks <aleksandarnp@gmail.com>
2021-06-26 00:10:29 +0300

    implementation of six dozens strategy

--- playFibonacciSixDozens.py
+++ playFibonacciSixDozens.py
@@ -4,28 +4,65 @@ from roulette import Roulette
 from playFibonacciThreeDozens import ThreeDozenStrategy
 
 class SixDozenStrategy(ThreeDozenStrategy):
-    
+
     def setupBetPots(self):
         # print("call in inherited")
-        super.setupBetPots()
-        print(self.firstDozenCounter)
+        super().setupBetPots()
+        # print(self.firstDozenCounter) # confirmation that inheritance work
 
-        self.topRowBet      = fibonacci(1) * self.bet
-        self.midRowBet      = fibonacci(1) * self.bet
-        self.lowRowBet      = fibonacci(1) * self.bet
+        self.topRowCounter = 1
+        self.midRowCounter = 1
+        self.lowRowCounter = 1
     #eof setupBetPots
+    
+    def placeBets(self):
+        super().placeBets()
 
-    def updateMoney(self, roll):
-        self.money = randint(self.startBudget/2, self.startBudget*2)
-        self.moneyChart.append(self.money)
-    # eof updateMoney
+        self.topRowBet      = self.pay(fibonacci(self.topRowCounter) * self.bet)
+        self.midRowBet      = self.pay(fibonacci(self.midRowCounter) * self.bet)
+        self.lowRowBet      = self.pay(fibonacci(self.lowRowCounter) * self.bet)
+    # eof placeBets
 
+    def updateAndCollect(self, roll):
+        super().updateAndCollect(roll)
+        
+        row = Roulette.getRow(roll)
+
+        topMultiplier = 0
+        topUpdate = 1
+        midMultiplier = 0
+        midUpdate = 1
+        lowMultiplier = 0
+        lowUpdate = 1
+        
+        if row == None:
+            pass
+        elif row == 'top':
+            topMultiplier = 3
+            topUpdate = -2
+        elif row == 'mid':
+            midMultiplier = 3
+            midUpdate = -2
+        elif row == 'low':
+            lowMultiplier = 3
+            lowUpdate = -2
+        else:
+            raise Exception("unexpected row returned = " + str(row))
+        
+        self.collect(self.topRowBet, topMultiplier)
+        self.topRowCounter = max(self.topRowCounter+topUpdate, 1)
+
+        self.collect(self.midRowBet, midMultiplier)
+        self.midRowCounter = max(self.midRowCounter+midUpdate, 1)
+
+        self.collect(self.lowRowBet, lowMultiplier)
+        self.lowRowCounter = max(self.lowRowCounter+lowUpdate, 1)
+    # eof updateAndCollect
 
 if __name__ == "__main__":
     strat = SixDozenStrategy(1000, 10, (0.66, 1.5))
     strat.play()
-    strat.isWin()
     strat.getPlot()
-
-    print("" % ())
+    
+    print("The result is a win: %s" % (strat.isWin()))
 #eof class tests
