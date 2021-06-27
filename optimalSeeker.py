fb08469d3f66f6ea4417bbdd66f3861292d2c3c9
Aleks <aleksandarnp@gmail.com>
2021-06-27 17:50:23 +0300

    added optimisation for money, not for percent

--- optimalSeeker.py
+++ optimalSeeker.py
@@ -7,64 +7,65 @@ import matplotlib.pyplot as plt
 
 import itertools
 
-# plotWinrate = []
+def findTopWinratePercentage():
 
-moneyOptions = list(range(50000, 100001, 1000))
-betOptions = [1, 5, 10, 20, 50, 100, 500, 1000, 2000, 5000]
-lossLimits = [x * 0.05 for x in range(20, 10, -1)]
-winLimits = [1 + (x * 0.05) for x in range(13, 2, -1)]
-# winLimits = [1.30]
+    moneyOptions = list(range(50000, 100001, 1000))
+    betOptions = [1, 5, 10, 20, 50, 100, 500, 1000, 2000, 5000]
+    lossLimits = [x * 0.05 for x in range(20, 10, -1)]
+    winLimits = [1 + (x * 0.05) for x in range(13, 2, -1)]
+    # winLimits = [1.30]
 
-# https://www.codegrepper.com/code-examples/python/python+generate+all+combinations+from+multiple+lists
-# a = [[1,2,3],[4,5,6],[7,8,9,10]]
+    # https://www.codegrepper.com/code-examples/python/python+generate+all+combinations+from+multiple+lists
+    # a = [[1,2,3],[4,5,6],[7,8,9,10]]
 
-a = [moneyOptions, betOptions, lossLimits, winLimits]
-testTuples = list(itertools.product(*a))
-# shuffle(testTuples)
+    a = [moneyOptions, betOptions, lossLimits, winLimits]
+    testTuples = list(itertools.product(*a))
+    # shuffle(testTuples)
 
-counter = 0
-testsLen = len(testTuples)
-bestWinrate = 0
-bestTuple = ()
-for aTuple in testTuples:
-    counter += 1
-    print("progress = %d of %d" % (counter, testsLen))
+    counter = 0
+    testsLen = len(testTuples)
+    bestWinrate = 0
+    bestTuple = ()
+    for aTuple in testTuples:
+        counter += 1
+        print("progress = %d of %d" % (counter, testsLen))
 
-    winrate = 0
-    wins = 0
-    avgLen = 0
-    tests = 10000
-    for i in range(1, tests):
-        # print("     progress = %d of %d" % (i, tests))
-        strat = ThreeDozenStrategy(aTuple[0], aTuple[1], (aTuple[2], aTuple[3]))
-        strat.play()
+        winrate = 0
+        wins = 0
+        avgLen = 0
+        tests = 10000
+        for i in range(1, tests):
+            # print("     progress = %d of %d" % (i, tests))
+            strat = ThreeDozenStrategy(aTuple[0], aTuple[1], (aTuple[2], aTuple[3]))
+            strat.play()
 
-        if strat.isWin():
-            wins = wins + 1
-            avgLen += len(strat.moneyChart)
+            if strat.isWin():
+                wins = wins + 1
+                avgLen += len(strat.moneyChart)
+            
+        #eof tests
+        winrate = (wins/tests)*100
         
-    #eof tests
-    winrate = (wins/tests)*100
-    
-    if winrate > bestWinrate:
-        avgLen = avgLen/wins 
-        bestWinrate = winrate
-        bestTuple = aTuple
-        print("%.2fpct winrate in %d rolls agv for params %s" % (winrate, avgLen, str(bestTuple)))
-        print("===")
-#eof testTuples    
+        if winrate > bestWinrate:
+            avgLen = avgLen/wins 
+            bestWinrate = winrate
+            bestTuple = aTuple
+            print("%.2fpct winrate in %d rolls agv for params %s" % (winrate, avgLen, str(bestTuple)))
+            print("===")
+    #eof testTuples    
 
 
-# plt.plot(plotWinrate, linewidth=2)
+    # plt.plot(plotWinrate, linewidth=2)
 
-# plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
-# plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
-# plt.xticks(fontsize=16, fontweight="bold")
-# plt.yticks(fontsize=16, fontweight="bold")
-# plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
-# plt.show()
+    # plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
+    # plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
+    # plt.xticks(fontsize=16, fontweight="bold")
+    # plt.yticks(fontsize=16, fontweight="bold")
+    # plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
+    # plt.show()
 
-# results
+    # results
+
 """
 1.25 gains 
 
@@ -91,7 +92,7 @@ aleks@aleks-Aspire-E5-572-2020:~/Development/Python/rouletteStrategies$ grep "pc
 69.05pct winrate in 38 rolls for (24000, 1, 0.15000000000000002, 1.3)
 
 bruteforce all no shuffle
-)
+
 0.02pct winrate in 138 rolls for (50000, 1, 1.0, 1.65)
 0.04pct winrate in 114 rolls for (50000, 1, 1.0, 1.3)
 10.13pct winrate in 108 rolls for (50000, 1, 0.9500000000000001, 1.65)
@@ -129,3 +130,66 @@ bruteforce all no shuffle
 74.57pct winrate in 48 rolls for (74000, 5, 0.55, 1.15)
 74.88pct winrate in 43 rolls for (93000, 10, 0.55, 1.15)
 """
+#eof findTopWinratePercentage
+
+def findTopMoneyGains():
+    '''compare ROI = money in vs money out
+    roi = total money out / total money in'''
+
+    moneyOptions = list(range(50000, 100001, 1000))
+    betOptions = [1, 5, 10, 20, 50, 100, 500, 1000, 2000, 5000]
+    lossLimits = [x * 0.05 for x in range(20, 10, -1)]
+    winLimits = [1 + (x * 0.05) for x in range(13, 2, -1)]
+
+    # https://www.codegrepper.com/code-examples/python/python+generate+all+combinations+from+multiple+lists
+    # a = [[1,2,3],[4,5,6],[7,8,9,10]]
+
+    a = [moneyOptions, betOptions, lossLimits, winLimits]
+    testTuples = list(itertools.product(*a))
+    shuffle(testTuples)
+
+    counter = 0
+    testsLen = len(testTuples)
+    bestROI = 0
+    totalMoneyIn = 0
+    totalMoneyOut = 0
+    bestTuple = ()
+    for aTuple in testTuples:
+        counter += 1
+        print("progress = %d of %d" % (counter, testsLen))
+
+        wins = 0
+        avgLen = 0
+        tests = 10000
+        for i in range(1, tests):
+            # print("     progress = %d of %d" % (i, tests))
+            strat = ThreeDozenStrategy(aTuple[0], aTuple[1], (aTuple[2], aTuple[3]))
+            strat.play()
+
+            totalMoneyIn += strat.startBudget
+            totalMoneyOut += strat.getMoney()
+
+            avgLen += len(strat.moneyChart)
+        #eof tests
+        roi = (totalMoneyOut/totalMoneyIn)*100
+        
+        if roi > bestROI:
+            avgLen = avgLen/tests
+            bestROI = roi
+            bestTuple = aTuple
+            print("%.2fpct roi (%d) in %d rolls agv for params %s" % (roi, (totalMoneyOut-totalMoneyIn)/tests, avgLen, str(bestTuple)))
+    #eof testTuples    
+
+
+    # # plt.plot(plotWinrate, linewidth=2)
+
+    # # plt.xlabel("Number of Games", fontsize=18, fontweight="bold")
+    # # plt.ylabel("Bankroll", fontsize=18, fontweight="bold")
+    # # plt.xticks(fontsize=16, fontweight="bold")
+    # # plt.yticks(fontsize=16, fontweight="bold")
+    # # plt.title("Bankroll Over Time", fontsize=22, fontweight="bold")
+    # # plt.show()
+
+# eof findTopMoneyGains
+
+findTopMoneyGains()
